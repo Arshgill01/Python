5dba332f1c5e6681f0e742b84a04b9b726e6b5ac
Arshdeep singh <arshgill6120@gmail.com>
2024-05-25 19:47:51 +0530

    did some editing

--- Calculator.py
+++ Calculator.py
@@ -2,7 +2,7 @@ x = float(input("What's x??"))
 y = float(input("What's y??"))
 
 z = round(x/y,2)
-print(f"{z:.2f}")
+print(z, sep="...")
 
-## Alt --  print(f"{z:.2f}")
+## Alt --  print(f"{z:}")
 
--- FirstCODE.py
+++ FirstCODE.py
@@ -1,5 +1,4 @@
-
-
-
-
+Mass = int(input("Whats the Mass of the object in Kgs? "))
+LightSpeed = int(3.000000000 * 3.000000000)
+print(Mass * LightSpeed)
 
